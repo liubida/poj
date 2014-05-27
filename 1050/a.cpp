2bed97d431af5411255de489f31dfa969545a669
bidaliu <bidaliu@sohu-inc.com>
2014-05-27 23:20:31 +0800

    1050 sloved

--- 1050/a.cpp
+++ 1050/a.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 #define INT_MIN 0x80000000
 
-int n;
-int *array;
-
-int sum(int start, int end) {
+int sum(int * array, int n, int start, int end) {
     int tmp = 0;
     for (int i=start; i<=end; i++) {
         if (i%n >= start%n && i%n <= end%n) {
@@ -17,14 +15,14 @@ int sum(int start, int end) {
     return tmp;
 }
 
-int calc() {
+int calc(int * array, int n) {
     int max = INT_MIN;
     int tmp = 0;
 
     for (int i=0; i<n*n; i++) {
         for (int j=i; j<n*n; j++) {
             if (i%n <= j%n) {
-                tmp = sum(i, j);
+                tmp = sum(array, n, i, j);
                 max = (max >= tmp) ? max : tmp;
                 //cout << max << ',' << i << ',' << j << endl;
             }
@@ -34,18 +32,20 @@ int calc() {
 }
 
 int main(){
+    int n = 0;
     cin >> n;
     if (n == 0) {
         cout << 0 << endl;
         return 0;
     }
-    array = new int(n * n);
+    int array[n*n];
     for (int x=0; x<n; x++) {
         for (int y=0; y<n; y++) {
+            //array[x * n + y] = rand();
             cin >> array[x * n + y];
         }
     }
-    calc();
+    calc(array, n);
 
     return 0;
 }
