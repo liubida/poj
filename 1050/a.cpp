3ad63ec4a3bc6bdb9372af4413ba931c9a59e65e
bidaliu <bidaliu@sohu-inc.com>
2014-05-27 10:47:02 +0800

    1050 ing

--- 1050/a.cpp
+++ 1050/a.cpp
@@ -2,18 +2,38 @@
 
 using namespace std;
 
-int n, *array;
+int n;
+short *array;
+
+int sum(int start, int end) {
+    int tmp = 0;
+    for (int i=start; i<=end; i++) {
+        if (i%n >= start%n) {
+            tmp += array[i];
+        }
+    }
+    return tmp;
+}
 
 int calc() {
-    for ( int y=0; y<n * n; y++) {
-        cout << " " << array[y];
+    int max = -999;
+    int tmp = 0;
+
+    for (int i=0; i<n*n; i++) {
+        for (int j=i; j<n*n; j++) {
+            if (i%n <= j%n) {
+                tmp = sum(i, j);
+                max = (max >= tmp) ? max : tmp;
+                //cout << max << ',' << i << ',' << j << endl;
+            }
+        }
     }
-    cout << endl;
+    cout << max << endl;
 }
 
 int main(){
     cin >> n;
-    array = new int(n * n);
+    array = new short(n * n);
     for (int x=0; x<n; x++) {
         for ( int y=0; y<n; y++) {
             cin >> array[x * n + y];
@@ -23,6 +43,5 @@ int main(){
 
     return 0;
 }
-
 
 
